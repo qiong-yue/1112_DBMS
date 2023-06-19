4b9f2be91b919c5aea8d3510042a02ed2e211c33
Joyce Chiang <107103035@nccu.edu.tw>
2023-06-20 00:01:11 +0800

    delete member

--- edit_profile.py
+++ edit_profile.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import session,redirect, url_for
+
 import sqlite3
 from flask import flash  # 利用flash  
 from app import app
+import sys
 
 
 #修改會員資料頁面( like edit in video ))
@@ -50,4 +53,29 @@ def edit():
             con.close()
         
 
-
+@app.route("/delete_member", methods=["POST", "GET"])
+def delete_member(): 
+    # print("not request method",  file=sys.stdout)
+    if request.method == "GET":
+        try:
+            print("in request method",  file=sys.stdout)
+            print("try to delete", file=sys.stdout);
+            #讀取需要修改會員的資料
+            email = session['email']
+            with sqlite3.connect('database.db') as con :
+                cur = con.cursor()    #以下是執行修改的動作
+                cur.execute("DELETE FROM User WHERE email = '" +email+"'")
+                con.commit()
+                session.clear()
+                print("delete success", file=sys.stdout);
+                return redirect(url_for("login")) # 利用flash 表示 修改成功 
+        except:
+            #sth wrong 
+            print("except");
+            con.rollback()
+            return "delete ERROR!" # 利用 flash 傳送修改錯誤
+        finally: 
+            con.close()
+            # return "close"
+    return 0
+        # return render_template("Log_In.html")

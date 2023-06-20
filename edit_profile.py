373d04c3aa1e5d783a2e8ba5cd979503d68b9820
Qiong-Tue <joanna4222@gmail.com>
2023-06-20 08:35:27 +0800

    edit_profile更新

--- edit_profile.py
+++ edit_profile.py
@@ -1,8 +1,6 @@
 from flask import Flask , redirect , url_for 
 from flask import render_template
-from flask import request
-from flask import session,redirect, url_for
-
+from flask import request, session
 import sqlite3
 import sys 
 from flask import flash  # 利用flash  
@@ -13,37 +11,28 @@ con = sqlite3.connect("database.db" , check_same_thread=False )
 @app.route('/member_profile' )
 def member_profile():
     return render_template("Member_Profile.html")
+
 #輸入修改的資料
 @app.route( '/edit' , methods=['POST' , 'GET'] )
 def edit():
+    print( "enter")
     if request.method == 'POST' :
-        email = request.form['email']   #找到要修改的email
+        email = session.get('email')
+        print(email)
+        pw = request.form['password']  
+        Bdate = request.form['bdate']
+        phone = request.form['phone']
+        addr = request.form['address']
         con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute(""" 
-            SELECT  
-                * 
-            FROM 
-                User 
-            WHERE 
-                email=? ;
-            """ , (email , ) ) #找到那人的資料
-        data = cur.fetchone()
-
-        if data :
-            pw = request.form['password']  
-            Bdate = request.form['bdate']
-            phone = request.form['phone']
-            addr = request.form['address']
-            cur = con.cursor()    #以下是執行修改的動作
-            cur.execute( "UPDATE User SET address=? , phone=? , Bdate=? , password=? , WHERE email=? " ,( addr , phone , Bdate , pw , email ) )
-            con.commit()
-            flash('Edit Success!')
-            return redirect(url_for("index"))
-        else:
-            return "email can not edit !!!"
+        cur = con.cursor()    #以下是執行修改的動作
+        cur.execute( "UPDATE User SET address=? , phone=? , Bdate=? , password=? , WHERE email=? " ,( addr , phone , Bdate , pw , email ) )
+        con.commit()
+        flash('Edit Success!')
+        return redirect(url_for("index"))
     return render_template("Log_In.html") #改網址名
         
+
+      
 @app.route("/delete_member", methods=["POST", "GET"])
 def delete_member(): 
     # print("not request method",  file=sys.stdout)
